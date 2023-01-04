ae00f6e45874fae2c673695952cb1fdd185ab7bd
ustyk1 <eugenia.ustyk@gmail.com>
2023-01-04 20:23:30 +0200

    add quick-transfer and transfers blocks

--- src/blocks/modules/quick-transfer/quick-transfer.js
+++ src/blocks/modules/quick-transfer/quick-transfer.js
@@ -1,13 +1,25 @@
-modules.define('quick-transfer', ['i-bem-dom'], function(provide, bemDom) {
+let index = 1;
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
+const on = (listener, selector, fn) => {
+	document.querySelectorAll(selector).forEach(item => {
+		item.addEventListener(listener, el => {
+			fn(el);
+		})
+	})
+}
 
+on('click', '.selectBtn', item => {
+  item.target.classList.toggle('toggle');
+    
+	const next = item.target.nextElementSibling;
+	next.classList.toggle('toggle');
+	next.style.zIndex = index++;
 });
+
+on('click', '.option', item => {
+	item.target.parentElement.classList.remove('toggle');
+
+	const parent = item.target.closest('.select').children[0];
+	parent.setAttribute('data-type', item.target.getAttribute('data-type'));
+	parent.innerText = item.target.innerText;
+})
