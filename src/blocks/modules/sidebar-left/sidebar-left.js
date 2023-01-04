f0a88515513d8a2814d554fd169d39a21d84163f
ustyk1 <eugenia.ustyk@gmail.com>
2023-01-04 18:45:16 +0200

    add header, bank-card and navigation-menu blocks

---
+++ src/blocks/modules/sidebar-left/sidebar-left.js
@@ -0,0 +1,13 @@
+modules.define('sidebar-left', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
---
+++ src/blocks/modules/sidebar-right/sidebar-right.js
@@ -0,0 +1,13 @@
+modules.define('sidebar-right', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
