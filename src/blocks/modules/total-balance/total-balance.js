6e23510eda31ef2b318318e7dd7c0e117b526caf
ustyk1 <eugenia.ustyk@gmail.com>
2023-01-09 23:21:24 +0200

    change total-balance block

--- src/blocks/modules/total-balance/total-balance.js
+++ src/blocks/modules/total-balance/total-balance.js
@@ -1,13 +1,20 @@
-modules.define('total-balance', ['i-bem-dom'], function(provide, bemDom) {
+const $selectDropdown = document.querySelector('.total-balance__balance-currency  .selectDropdown');
+const $allBalanceSum = Array.from(document.querySelectorAll('.total-balance__sum .sum'));
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
+$selectDropdown.addEventListener('click', function (e) {
+    const $option = e.target;
+    const optionDataAttr = $option.getAttribute('data-option');
+
+    for (let totalSum of  $allBalanceSum) {
+        const totalSumId = totalSum.getAttribute('id');
+
+        if (totalSumId === optionDataAttr) {
+            totalSum.classList.remove('inv');
+            totalSum.classList.add('vis');
+        } else {
+            totalSum.classList.remove('vis');
+            totalSum.classList.add('inv');
         }
     }
-}));
-
 });
+
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,4 @@
 import "%modules%/header/header";
 import "%modules%/footer/footer";
 import "%modules%/quick-transfer/quick-transfer";
+import "%modules%/total-balance/total-balance";
